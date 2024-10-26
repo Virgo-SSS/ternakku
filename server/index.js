1757afaa28ade3dae04c85e1f2744fd504bb4981
virgo <virgostevanus27@gmail.com>
2024-10-26 14:25:02 +0700

    setup express and install nodemon

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
-console.log('Server is runningss');
+const express = require('express');
+
+const app = express();
+
+// listen to port 3000
+app.listen(3000, () => {
+  console.log('Server is running on port 3000 and ready to accept requests!');
+});
