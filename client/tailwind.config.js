08d5b5995fe8419440525cff09c287af406c1cb1
virgo <virgostevanus27@gmail.com>
2024-11-09 01:52:56 +0700

    fix indendation for file vite.config.js, postcss.config.js, tailwind.config.js and delete license file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['"Public Sans"', 'sans-serif'], // Add Public Sans as the default sans font
-      },
-      colors: {
-        'tw-primary': '#93A603', // Customize this color code as your primary color
-        base: '#F5F5F9', // Customize this color code as your primary color
-        "basecontent": '#384551'
-      },
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['"Public Sans"', 'sans-serif'], // Add Public Sans as the default sans font
+            },
+            colors: {
+                'tw-primary': '#93A603', // Customize this color code as your primary color
+                base: '#F5F5F9', // Customize this color code as your primary color
+                "basecontent": '#384551'
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
