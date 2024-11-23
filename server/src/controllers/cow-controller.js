2b840371f87cae94fdf733cc9eff00002521bb98
virgo <virgostevanus27@gmail.com>
2024-11-23 22:59:49 +0700

    add cows store function

---
+++ server/src/controllers/cow-controller.js
@@ -0,0 +1,38 @@
+import CowModel from '../models/cow.js';
+
+const store =   async (req, res) => {
+    try {
+        const { body } = req;
+
+        const [rows] = await CowModel.create({
+            name: body.name,
+            status: body.status,
+            gender: body.gender,
+            birth_date: body.birth_date,
+            weight: body.weight,
+            type: body.type,
+            is_bought: body.is_bought,
+        });
+
+        res.status(201)
+        .json({
+            message: 'Cow created successfully',
+            data: {
+                id: rows.insertId,
+                name: body.name,
+                status: body.status,
+                gender: body.gender,
+                birth_date: body.birth_date,
+                weight: body.weight,
+                type: body.type,
+                is_bought: body.is_bought
+            }   
+        })
+    } catch (error) {
+        res.status(500).send({message: error.message});
+    }
+};
+
+export default {
+    store,
+}
--- server/src/routes/api-v1.js
+++ server/src/routes/api-v1.js
@@ -3,11 +3,13 @@ import express from 'express';
 import LoginController  from '../controllers/login-controller.js';
 import RegisterController from '../controllers/register-controller.js';
 import WorkerController from '../controllers/worker-controller.js';
+import CowController from '../controllers/cow-controller.js';
 
 const router = express.Router();
 
 router.post('/login', LoginController.login);
 router.post('/register', RegisterController.register);
 router.post('/worker', WorkerController.store);
+router.post('/cow', CowController.store);
 
 export default router;
